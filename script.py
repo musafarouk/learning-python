9cd4b5b832fd1e98139ffe368fe84bfb15997727
Devlin Blake <R3N3GAD3@DevlinBlake.local>
2018-05-10 10:49:50 +0100

    Strings, floats, booleans and print syntax

---
+++ script.py
@@ -0,0 +1,17 @@
+#Regular recaps on Python Programming
+#SOme of the codes might be in Python 2 & Python 3
+
+'''Datatypes'''
+#Strings
+name = "Hello there"
+
+#Floats
+number = 5
+
+#Boolean
+Sunny = True
+Rainy = False
+
+'''Displaying on the console'''
+print "Welcome Text"
+
